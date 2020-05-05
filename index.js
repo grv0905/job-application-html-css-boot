37678e240ce3746821bad350e8a53578637cb2c4
Gaurav Sharma <kvdgaurav@gmail.com>
2020-05-05 09:40:00 -0700

    commit

---
+++ index.js
@@ -0,0 +1,8 @@
+  $(function() {
+            $( "#datepicker-13").datepicker();
+            $( "#datepicker-from-1").datepicker();
+            $( "#datepicker-to-1").datepicker();
+                        $( "#datepicker-from-2").datepicker();
+            $( "#datepicker-to-2").datepicker();
+         	$( "#datepicker-joining").datepicker();
+         });
